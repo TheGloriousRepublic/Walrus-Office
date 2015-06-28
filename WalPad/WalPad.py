ce38bc89394258c76fcdc138a8061aba8b2912fa
Nathan <nathantronnes@gmail.com>
2015-06-28 14:24:27 -0500

    file menu shell
    Also, made the WalPad have no command window

--- WalPad/WalPad.py
+++ WalPad/WalPad.py
@@ -7,10 +7,28 @@ class textpad(tk.Tk):
         self.st = ScrolledText()
         self.st.pack()
 
-        menubar = tk.Menu(self)
-        menubar.add_command(label="Hello!")
+        self.menubar = tk.Menu(self)
+        
+        self.filemenu = tk.Menu(self)
+        self.filemenu.add_command(label="New Window", command=self.new)
+        self.filemenu.add_command(label="Save", command=self.savefile)
+        self.filemenu.add_command(label="Save As", command=self.savefileas)
+        self.filemenu.add_command(label="Open", command=self.openfile)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
 
-        self.config(menu=menubar)
+        self.config(menu=self.menubar)
+    
+    def savefile(self, *args, **kwargs):
+        pass
+
+    def savefileas(self, *args, **kwargs):
+        pass
+
+    def openfile(self, *args, **kwargs):
+        pass
+
+    def new(self, *args, **kwargs):
+        pass
 
 t = textpad()
 
