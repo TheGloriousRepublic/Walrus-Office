5d2849b405b20eaba90d1934f37858f54dd2afc5
Nathan <nathantronnes@gmail.com>
2015-06-28 15:40:48 -0500

    Autoresize of text widget
    Yaaaaaaay

--- WalPad/WalPad.py
+++ WalPad/WalPad.py
@@ -7,7 +7,7 @@ class textpad(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.st = ScrolledText()
-        self.st.pack()
+        self.st.pack(fill=tk.BOTH, expand=1)
 
         self.fileloc = None
         
@@ -15,28 +15,31 @@ class textpad(tk.Tk):
         
         self.filemenu = tk.Menu(self)
         self.filemenu.add_command(label='New Window', command=self.new)
-        self.filemenu.add_command(label='Save', command=self.savefile)
-        self.filemenu.add_command(label='Save As', command=self.savefileas)
-        self.filemenu.add_command(label='Open', command=self.openfile)
-        self.menubar.add_cascade(label='File', menu=self.filemenu)
+        self.filemenu.add_command(label='Save',       command=self.savefile)
+        self.filemenu.add_command(label='Save As',    command=self.savefileas)
+        self.filemenu.add_command(label='Open',       command=self.openfile)
+        self.menubar.add_cascade(label='File',        menu=self.filemenu)
 
         self.editmenu = tk.Menu(self)
-        self.editmenu.add_command(label='Undo')
-        self.editmenu.add_command(label='Redo')
-        self.editmenu.add_command(label='Cut', command=self.cuttext)
-        self.editmenu.add_command(label='Copy', command=self.copytext)
-        self.editmenu.add_command(label='Glue', command=self.gluetext)
-        self.editmenu.add_command(label='Paste', command=self.pastetext)
-        self.menubar.add_cascade(label='Edit', menu=self.editmenu)
+        self.editmenu.add_command(label='Undo',       command=self.undochange)
+        self.editmenu.add_command(label='Redo',       command=self.redochange)
+        self.editmenu.add_command(label='Cut',        command=self.cuttext)
+        self.editmenu.add_command(label='Copy',       command=self.copytext)
+        self.editmenu.add_command(label='Glue',       command=self.gluetext)
+        self.editmenu.add_command(label='Paste',      command=self.pastetext)
+        self.menubar.add_cascade(label='Edit',        menu=self.editmenu)
 
         self.st.bind('<ButtonRelease-3>', self.rclick)
-        self.st.bind('<Control-c>', self.copytext)
-        self.st.bind('<Control-x>', self.cuttext)
-        self.st.bind('<Control-g>', self.gluetext)
-        self.st.bind('<Control-v>', self.pastetext)
+
+        self.bind('<ButtonRelease-1>', self.update)
 
         self.config(menu=self.menubar)
-    
+
+    def update(self, *args):
+        print('Update!')
+        self.st.update()
+        
+        
     def savefile(self, *args, **kwargs):
         if self.fileloc:
             open(self.fileloc, 'w+').write(self.getcontent())
@@ -95,6 +98,15 @@ class textpad(tk.Tk):
 
     def rclick(self, event):
         self.editmenu.post(event.x_root, event.y_root)
+
+    def exitprogram(self):
+        self.exit()
+
+    def undochange(self, *args):
+        pass
+
+    def redochange(self, *args):
+        pass
 
 t = textpad()
 
